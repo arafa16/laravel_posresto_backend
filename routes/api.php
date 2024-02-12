b88023b495689bea194d558534496f3ab317093f
ara fa adri <arafa.sco@gmail.com>
2024-02-12 23:01:19 +0700

    create api done

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//post login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+//get product
+Route::apiResource('/api-products', App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
+//get category
+Route::apiResource('/api-categories', App\Http\Controllers\Api\CategoryController::class)->middleware('auth:sanctum');
