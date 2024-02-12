3802dbded57a6ee28768f47780ec7a51d1ce7a01
ara fa adri <arafa.sco@gmail.com>
2024-02-12 23:10:59 +0700

    perbaikan nama list

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -17,8 +17,8 @@
                 </div>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                    <div class="breadcrumb-item"><a href="#">Users</a></div>
-                    <div class="breadcrumb-item">All Users</div>
+                    <div class="breadcrumb-item"><a href="#">Products</a></div>
+                    <div class="breadcrumb-item">All Product</div>
                 </div>
             </div>
             <div class="section-body">
